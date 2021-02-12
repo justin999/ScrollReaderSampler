dcae037d722309433faa71423f777a324ecd6e4d
Justin Sato <koichi.sato.aow@gmail.com>
2021-02-12 09:51:57 +0900

    Initial Commit

---
+++ ScrollReaderSampler/ScrollReaderSamplerApp.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollReaderSamplerApp.swift
+//  ScrollReaderSampler
+//
+//  Created by Justin Sato on 2021/02/12.
+//  
+//
+
+import SwiftUI
+
+@main
+struct ScrollReaderSamplerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
